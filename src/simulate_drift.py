6be0f0a3d56cc44a1ad12a155c2c32c06266970c
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2025-09-14 01:34:40 -0700

    fixing save name for drifted data

--- src/simulate_drift.py
+++ src/simulate_drift.py
@@ -2,4 +2,4 @@
 import pandas as pd
 df = pd.read_csv('data/titanic.csv')
 df['Age'] = df['Age'] * 1.05  # Increase all ages by 5%
-df.to_csv('data/train_drifted.csv', index=False)
+df.to_csv('data/titanic_drifted.csv', index=False)
