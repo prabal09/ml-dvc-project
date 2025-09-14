6d0a57b6a1d201bd5b406dea11ae50204eb1451a
Prabal Dutta <prabaldutta.iitr@gmail.com>
2025-09-14 09:15:53 +0000

    simulating 15 percent age drift

--- src/simulate_drift.py
+++ src/simulate_drift.py
@@ -1,5 +1,5 @@
 # simulate_drift.py
 import pandas as pd
 df = pd.read_csv('data/titanic.csv')
-df['Age'] = df['Age'] * 1.03  # Increase all ages by 3%
+df['Age'] = df['Age'] * 1.15  # Increase all ages by 15%
 df.to_csv('data/titanic_drifted.csv', index=False)
