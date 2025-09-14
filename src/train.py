cab3cc59bc41dcff7c27ebd3718a817a87771511
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2025-09-13 23:51:17 -0700

    updated path to training file

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score
 
 # Load the dataset
 # You'll need to have the titanic.csv file in the same directory or specify the correct path
-titanic = pd.read_csv('titanic.csv')
+titanic = pd.read_csv('data/titanic.csv')
 
 # Data Cleaning and Preparation (basic example)
 # Handle missing values (e.g., fill 'Age' with the mean, drop 'Cabin')
